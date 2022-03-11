1c01ebdd96fc739e1904175208d6041fa4ddb864
yashpatil007 <yashpatil85325@gmail.com>
2022-03-11 10:46:28 +0530

    ADDED: Line Comparision Problem For Institute

---
+++ src/com/infogalaxy/LineProblem.java
@@ -0,0 +1,7 @@
+package com.infogalaxy;
+
+public class LineProblem {
+    public static void main(String args[]){
+        System.out.println("Welcome to Line Comparision Problem Developed by Yash");
+    }
+}
